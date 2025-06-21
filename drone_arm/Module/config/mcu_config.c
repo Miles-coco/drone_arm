8959cf09e21516057e39bcb3328c73a7e778255d
皮程锦 <14843461+pi-chengjin@user.noreply.gitee.com>
2025-06-21 18:32:26 +0800

    增加了MCU初始化模块

---
+++ drone_arm/Module/config/mcu_config.c
@@ -0,0 +1,14 @@
+#include "mcu_config.h"
+#include "driver.h"
+#include "task.h"
+#include "dm_ctrl.h"
+#include "remote_control.h"
+
+void MCU_init(void)
+{
+    task_init();
+    //RC_init();
+    can_driver_init();
+    dm4310_motor_init();
+
+}
